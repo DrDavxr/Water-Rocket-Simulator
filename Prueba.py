9cdd4752394c90b9bcf2ab25568579b18c640a0b
Raul98 <100363363@alumnos.uc3m.es>
2020-02-08 17:29:24 +0100

    Delete Prueba.py

--- Prueba.py
+++
@@ -1,8 +0,0 @@
-from Equations_of_Tank import TankFlow
-import numpy as np
-
-state_vector = np.array([3.37])
-
-tank = TankFlow(state_vector, 0.1, 0.102, 0.008, 101325, 1000, 1.4)
-
-density = tank.DensityComp(0.1)
